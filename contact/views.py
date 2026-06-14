63e2c14fc06dfc6ba2dd1cae97c32ba6d9321158
Vanathi Muthu <vanathivanathi52@gmail.com>
2026-06-14 17:13:04 +0530

    fix middleware

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.core.mail import send_mail
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -8,55 +6,28 @@ from .models import ContactMessage
 from .serializers import ContactSerializer
 
 
-class ContactViewSet(
-    ModelViewSet
-):
+class ContactViewSet(ModelViewSet):
 
-    queryset = (
-        ContactMessage.objects.all()
-    )
+    queryset = ContactMessage.objects.all()
+    serializer_class = ContactSerializer
 
-    serializer_class = (
-        ContactSerializer
-    )
+    def create(self, request, *args, **kwargs):
 
-    def create(
-        self,
-        request,
-        *args,
-        **kwargs
-    ):
-
-        serializer = (
-            self.get_serializer(
-                data=request.data
-            )
+        serializer = self.get_serializer(
+            data=request.data
         )
 
         serializer.is_valid(
             raise_exception=True
         )
 
-        contact_message = serializer.save()
-        sender_email = contact_message.email
-        body = (
-            f"Name: {contact_message.name}\n"
-            f"Email: {sender_email}\n\n"
-            f"Message:\n{contact_message.message}"
-        )
-
-        try:
-            send_mail(
-                "Portfolio Contact",
-                body,
-                settings.DEFAULT_FROM_EMAIL,
-                [settings.EMAIL_HOST_USER],
-                fail_silently=False,
-            )
-        except Exception as e:
-            print("EMAIL ERROR:", str(e))
+        serializer.save()
 
         return Response(
-            serializer.data,
+            {
+                "success": True,
+                "message": "Contact message submitted successfully.",
+                "data": serializer.data
+            },
             status=status.HTTP_201_CREATED
         )
