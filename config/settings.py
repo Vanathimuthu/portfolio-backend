672b973a844a5b4b13890aafa9e5ff54f161a2a3
Vanathi Muthu <vanathivanathi52@gmail.com>
2026-06-14 17:49:22 +0530

    fix middleware

--- config/settings.py
+++ config/settings.py
@@ -168,14 +168,17 @@ STATICFILES_STORAGE = (
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+import os
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'vanathivanathi52@gmail.com'
-EMAIL_HOST_PASSWORD = 'whuv owvs ooip uvfo'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
