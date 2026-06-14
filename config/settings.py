ed81c45237b9a7b35208e3140cc147560fc309f7
Vanathi Muthu <vanathivanathi52@gmail.com>
2026-06-14 14:49:18 +0530

    fix middleware

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.security.SecurityMiddleware"
+    "django.middleware.security.SecurityMiddleware",
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
